c1334807fb26f2ad05474e01718d2d6fd7099f41
MuffinHavlop <101418643+MuffinHavlop@users.noreply.github.com>
2024-12-14 18:24:32 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function rightAnswer(){
+    guess.textContent = `CORRECT!!!👍👍🔥🔥`;
+}
+function wrongAnswer(){
+    guess.textContent = 'WRONG👎👎!!! GUES AGAIN';
+}
+function rightAnswer2(){
+    guess2.textContent = `CORRECT!!!👍👍🔥🔥`;
+}
+function wrongAnswer2(){
+    guess2.textContent = 'WRONG👎👎!!! GUES AGAIN';
+}
+function rightAnswer3(){
+    guess3.textContent = `CORRECT!!!👍👍🔥🔥`;
+}
+function wrongAnswer3(){
+    guess3.textContent = 'WRONG👎👎!!! GUES AGAIN';
+}
